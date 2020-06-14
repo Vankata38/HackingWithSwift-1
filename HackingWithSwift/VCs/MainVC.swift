89e6da3302c9084acdba1e507d2c7e90d1d5ae22
Ivan Terziev <ivanterziev38@gmail.com>
2020-06-15 00:58:05 +0300

    Converted to using a tableView, included a few pictures, and made the tableView create as much cells as there are pictures.

--- HackingWithSwift/VCs/MainVC.swift
+++ HackingWithSwift/VCs/MainVC.swift
@@ -8,17 +8,37 @@
 
 import UIKit
 
-class MainVC: UIViewController {
+class MainVC: UITableViewController {
 
     // MARK: - Properties
-    
+    var pictures = [String]()
+
     
     // MARK: - App cycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        print(pictures)
+    }
+    
+    // Set the number of rows
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
 
+    // Create the cells
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
     }
     
     
